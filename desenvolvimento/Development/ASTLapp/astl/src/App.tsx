2502b32d8ba3e11f48656954d046a9ba72e66ff8
ThieresKB <thieresmda@gmail.com>
2023-09-12 21:56:56 -0300

    Atualizado

--- desenvolvimento/Development/ASTLapp/astl/src/App.tsx
+++ desenvolvimento/Development/ASTLapp/astl/src/App.tsx
@@ -1,37 +1,134 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import googleImg from './Content/Img/google.png'
+import githubImg from './Content/Img/github.png'
+
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach( (eyeIcon:any) => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach( (password:any) => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})      
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms?.classList.toggle("show-signup");
+    })
+})
 
 function App() {
   return (
-    <div className="container">
-        {/* <!-- Login Form--> */}
-        <div className="login-form">
-            <div className="title">Login</div>
+    <section className="container forms">
+    <div className="form login">
+        <div className="form-content">
+            <header>Login</header>
             <form action="#">
-                {/* <!-- E-mail --> */}
-                <div className="input-box">
-                    <i className="fa fa-envelope"></i>
-                    <input type='text' placeholder='E-mail' required />
+                <div className="field input-field">
+                    <input type="email" placeholder="Email" className="input" />
                 </div>
-                {/* <!-- Password --> */}
-                <div className="input-box">
-                    <i className="fa fa-lock"></i>
-                    <input type="password" placeholder="Senha" required />
+
+                <div className="field input-field">
+                    <input type="password" placeholder="Senha" className="password" />
+                    <i className='bx bx-hide eye-icon'></i>
                 </div>
-                {/* <!-- Forgot Password --> */}
-                <div className="forgot"><a href="#">Esqueceu a sua senha?</a></div>
-                {/* <!-- Enter --> */}
-                <div className="input-box">
-                    <input type="submit" value="Entrar" />
+
+                <div className="form-link">
+                    <a href="#" className="forgot-pass">Esqueceu a senha?</a>
                 </div>
-                <div className="signup-text">
-                    Não tem uma conta? <a href="sign up.html">Crie uma agora!</a>
+
+                <div className="field button-field">
+                    <button>Login</button>
                 </div>
             </form>
+
+            <div className="form-link">
+                <span>Não consegue acessar a conta? <a href="#" className="link signup-link">Cadastrar-se</a></span>
+            </div>
         </div>
-        {/* <!-- End of Login --> */}
+
+        <div className="line"></div>
+
+        <div className="media-options">
+            <a href="#" className="field facebook">
+                {/* <i className='bx bxl-facebook facebook-icon'></i> */}
+                <img src={githubImg}  alt="" className="github-img" />
+                <span>Acesse com o GitHub</span>
+            </a>
+        </div>
+
+        <div className="media-options">
+            <a href="#" className="field google">
+                <img src={googleImg}  alt="" className="google-img" />
+                <span>Acesse com o Google</span>
+            </a>
+        </div>
+
     </div>
+
+    {/* <!-- Signup Form --> */}
+
+    <div className="form signup">
+        <div className="form-content">
+            <header>Signup</header>
+            <form action="#">
+                <div className="field input-field">
+                    <input type="email" placeholder="Email" className="input" />
+                </div>
+
+                <div className="field input-field">
+                    <input type="password" placeholder="Create password" className="password" />
+                </div>
+
+                <div className="field input-field">
+                    <input type="password" placeholder="Confirm password" className="password" />
+                    <i className='bx bx-hide eye-icon'></i>
+                </div>
+
+                <div className="field button-field">
+                    <button>Signup</button>
+                </div>
+            </form>
+
+            <div className="form-link">
+                <span>Already have an account? <a href="#" className="link login-link">Login</a></span>
+            </div>
+        </div>
+
+        <div className="line"></div>
+
+        <div className="media-options">
+            <a href="#" className="field facebook">
+                {/* <i className='bx bxl-facebook facebook-icon'></i> */}
+                <img src={githubImg}  alt="" className="github-img" />
+                <span>Login with Facebook</span>
+            </a>
+        </div>
+
+        <div className="media-options">
+            <a href="#" className="field google">
+                <img src={googleImg} alt="" className="google-img" />
+                <span>Login with Google</span>
+            </a>
+        </div>
+
+    </div>
+</section>
   );
 }
 
